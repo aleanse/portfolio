dab2534d92ea2afe7a6c732006ccf3b4da9dfda8
aleanse <aleanse_54@hotmail.com>
2024-04-17 09:40:47 -0300

    jads

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,18 +3,15 @@ function Footer() {
   return (
     <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
-        Designed and Coded by{" "}
+        Feito por {" "}
         <a
           className="font-medium"
-          href="https://pavanmg.in"
+          href="https://github.com/aleanse"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Pavan MG
+          Aleanse
         </a>{" "}
-        with
-        <span className="text-gradient font-medium"> Love</span> &
-        <span className="text-gradient font-medium"> Coffee</span>
       </p>
     </footer>
   );
