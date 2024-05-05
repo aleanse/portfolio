a33b1df1132a0f06097e31f713f766c7504e7f56
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-05-05 20:46:49 -0300

    dda

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,23 +16,7 @@ function Header() {
         <NavLink to="/">
           <img className="w-34 h-24" src={logos.logogradient} alt="logo" />
         </NavLink>
-        <div onClick={toggleClass} className="cursor-pointer">
-          <svg
-            className="stroke-white md:hidden"
-            width="25"
-            height="20"
-            viewBox="0 0 16 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.4375 1.3125H14.5625M1.4375 11.3125H14.5625H1.4375ZM1.4375 6.3125H14.5625H1.4375Z"
-              strokeWidth="1.875"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
+        
       </div>
       <nav className={` ${!isOpen ? "hidden" : null} text-center md:flex justify-between`}>
         <ul className="text-light-content font-medium md:flex items-center md:space-x-5 md:mr-10">
@@ -41,22 +25,22 @@ function Header() {
               Pagina inicial
             </NavLink>
           </li>
-          <li className="pb-1 md:pb-0">
+          <li className="inicial">
             <NavLink to="/about" onClick={toggleClass}>
               Sobre mim
             </NavLink>
           </li>
-          <li className="pb-1 md:pb-0">
+          <li className="inicial">
             <NavLink to="/technologies" onClick={toggleClass}>
               Tecnologias
             </NavLink>
           </li>
-          <li className="pb-1 md:pb-0">
+          <li className="inicial">
             <NavLink to="/projects" onClick={toggleClass}>
               Projetos
             </NavLink>
           </li>
-          <li>
+          <li className="inicial">
             <NavLink to="/contact" onClick={toggleClass}>
               Contato
             </NavLink>
