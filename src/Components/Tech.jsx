ef8b6ec349a0de84dbf460764c34b6890c8f2d19
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-05-06 20:36:57 -0300

    dkas

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -12,9 +12,9 @@ const Tech = () => {
         <h2 className={`${styles.sectionHeadTextLight} text-light-heading text-center`}>Tecnologias</h2>
       </motion.div>
 
-      <div className="grid grid-cols-4 gap-20 mt-14">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8 lg:gap-20 mt-14">
         {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
+          <div className="w-28 h-28 " key={technology.name}>
             <BallCanvas icon={technology.icon} />
           </div>
         ))}
